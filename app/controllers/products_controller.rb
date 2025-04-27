0a6e216bcc3e99b52088ced43db847bce3020dc9
MrElyazid <elyazid.assouli@gmail.com>
2025-04-27 18:32:41 +0100

    search

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,13 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
+    if params[:query].present?
+      # Basic case-insensitive search using LIKE
+      # Note: For larger datasets, consider using a dedicated search gem (e.g., Ransack, PgSearch)
+      # or full-text search capabilities of the database for better performance.
+      @products = Product.where("LOWER(name) LIKE ?", "%#{params[:query].downcase}%")
+    else
+      @products = Product.all
+    end
   end
 
   def show
